b5947ec1ed434d308cc922f5b981aa768b963152
Seena Rowhani <seenarowhani95@gmail.com>
2018-04-28 16:23:54 -0400

    p

--- app/pods/overview/controller.js
+++ app/pods/overview/controller.js
@@ -1,4 +1,14 @@
 import Controller from '@ember/controller';
 
+import { computed } from '@ember/object';
+
+import faker from 'faker';
+
 export default Controller.extend({
+  userListItems: computed('model', function () {
+    const model = this.get('model');
+    return Object.keys(model).map(key => Object.assign({
+      profile_img: faker.image.avatar()
+    }, model[key]));
+  })
 });
