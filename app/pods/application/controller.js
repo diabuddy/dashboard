b2ff98e4c91c6716acbb499bc9d363bdb19f8f55
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-04-28 14:40:07 -0400

    additions to sidenav

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -4,11 +4,46 @@ export default Controller.extend({
     loggedIn: true,
     sidenav: false,
     lockedSidenav: true,
+    expandBuds: false,
+    buddies: [
+        { 
+            name: "Steve",
+            uid:"1"
 
+        },
+        { 
+            name: "Mary",
+            uid:"2"
+        },
+        { 
+            name: "Calvin",
+            uid:"3"
+        },
+    ],
     actions: {
         toggleSidenav () {
             this.toggleProperty('sidenav')
             this.toggleProperty('lockedSidenav')
+        },
+        toggleExpandedItem (item) {
+            this.toggleProperty(item)
+            console.log(item, this.get(item))
+        },
+        selectedBuddies (buddyId) {
+            let buddies = this.get('buddies')
+            for(let i = 0; i< buddies.length;i++){
+                if(buddies[i].uid == buddyId){
+                    if(buddies[i].selected == undefined || buddies[i].selected == true){
+                        buddies[i].selected = false
+                    } else {
+                        buddies[i].selected = true
+                    }
+                    break;
+                }
+            }
+            console.log(buddies)
+            this.set('buddies', buddies)
+            console.log(this.get('buddies'))
         }
     }
 });
