629a8551a47667244250dc8c27884348f93134d4
Seena Rowhani <seenarowhani95@gmail.com>
2018-04-28 13:42:25 -0400

    p

--- app/pods/session/service.js
+++ app/pods/session/service.js
@@ -2,5 +2,12 @@ import Service from '@ember/service';
 import { computed } from '@ember/object';
 
 export default Service.extend({
-
+  beginAuth () {
+    ui.start('#firebaseui-auth-container', {
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      // Other config options...
+    });
+  }
 });
