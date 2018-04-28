0940063e1b814de46dd96042a2d93fc3ac9cd6b6
Seena Rowhani <seenarowhani95@gmail.com>
2018-04-28 10:47:53 -0400

    chore(scaffold): adds routes

---
+++ app/pods/application/controller.js
@@ -0,0 +1,10 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  num: 0,
+  actions: {
+    doThis () {
+
+    }
+  }
+});
--- app/router.js
+++ app/router.js
@@ -7,6 +7,8 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('dashboard');
+  this.route('login');
 });
 
 export default Router;
