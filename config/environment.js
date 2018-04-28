917cbb4a39f006d242ca75e1f457d13c0ec9aba7
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-04-28 10:33:58 -0400

    pods

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'dashboard',
+    podModulePrefix: 'dashboard/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
