10457827c198f38114b9c62cd21b9d26fedaa860
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-04-28 23:04:22 -0400

    change data

--- app/pods/dashboard/controller.js
+++ app/pods/dashboard/controller.js
@@ -6,16 +6,16 @@ export default Controller.extend({
         label: "24 Hours",
         value: 24
     },
-    mockUsers: ['3Nwq2LuIs9QaVxzEo57szgBC7ME2', '68NeeBd25GZ580xH4ZSFc415UWk2', 'E4HHocNr7yTwBBbPpdOWX6YyVH12'],
-    userData: computed('model', function () {
-        const model = this.get('model');
-        const buddies = this.get('mockUsers')
-        let data = []
-        //debugger
-        for(let x of buddies){
-            data.push(model[x])
-        }
+    selectedBuddies: [{'3Nwq2LuIs9QaVxzEo57szgBC7ME2': "Steve Schmid"}, {'68NeeBd25GZ580xH4ZSFc415UWk2': "Mat Schmid"}, {'E4HHocNr7yTwBBbPpdOWX6YyVH12': "Mary Schmid"}],
 
+    userData: computed('model', 'selectedBuddies', function () {
+        const model = this.get('model');
+        const buddies = this.get('selectedBuddies')
+        let data = []
+        for(let x of buddies){
+            data.push(model[Object.keys(x)[0]])
+        }
+        console.log(data)
         return data
       })
 });
