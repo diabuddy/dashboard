fa04ce1990fd0523848a15e2ca91277bde307984
Seena Rowhani <seenarowhani95@gmail.com>
2018-04-29 01:00:44 -0400

    boy

--- app/pods/components/map-element/component.js
+++ app/pods/components/map-element/component.js
@@ -1,5 +1,9 @@
 import Component from '@ember/component';
-
 export default Component.extend({
-  tagName: ''
+  tagName: '',
+  didInsertElement () {
+    window.requestAnimationFrame(() => {
+      this.set('_shouldRenderMap', true)
+    })
+  }
 });
