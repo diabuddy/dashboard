93cd13966c76cf6e9fe9cbd47f0ccb718784c2ef
Seena Rowhani <seenarowhani95@gmail.com>
2018-04-29 11:49:41 -0400

    p

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    fingerprint: {
+       exclude: ['assets/images/**/*']
+     }
     // Add options here
   });
 
