924ba998b7bd46b59703c19a792e962348a81070
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-04-29 03:40:28 -0400

    working score progress

--- app/pods/dashboard/controller.js
+++ app/pods/dashboard/controller.js
@@ -1,5 +1,5 @@
 import Controller from '@ember/controller';
-import { observer } from '@ember/object';
+import { observer, computed } from '@ember/object';
 
 export default Controller.extend({
   selectedRange: {
@@ -7,6 +7,34 @@ export default Controller.extend({
     value: 24
   },
   mockUsers: ['3Nwq2LuIs9QaVxzEo57szgBC7ME2', '68NeeBd25GZ580xH4ZSFc415UWk2', 'E4HHocNr7yTwBBbPpdOWX6YyVH12'],
+  userScores: computed( 'model', function() {
+    const model = this.get('model');
+    const buddies = this.get('mockUsers')
+    let data = []
+    for (let id of buddies) {
+      data.push(Object.assign({ id }, model[id]))
+    }
+    let scores = []
+    for(let person of data){
+        let score = 0
+        if(person != undefined){
+            for (let day in person.history){
+                for (let event in person.history[day]){
+                    if(person.history[day][event].eventType == "insulinUpdate"){
+                        score+= 500
+                    }else if(person.history[day][event].eventType == "glucoseUpdate"){
+                        score+= 500
+                    }else{
+                        score+= 500
+                    }
+                }
+            }
+            scores.push({name: person.name, score: score})
+        }
+    }
+    console.log(scores)
+    return scores
+  }),
   dataChange: observer('model', function () {
     const model = this.get('model');
     const buddies = this.get('mockUsers')
